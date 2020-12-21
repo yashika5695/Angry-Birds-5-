d08efffd8c9185315752c670872026e8825b99ad
Yashika <rahulagarwal5695@gmail.com>
2020-12-21 14:45:01 +0530

    Slingshot

--- Ground.js
+++ Ground.js
@@ -11,7 +11,9 @@ class Ground {
     display(){
       var pos =this.body.position;
       rectMode(CENTER);
+      stroke("darkgreen")
       fill("brown");
       rect(pos.x, pos.y, this.width, this.height);
+     
     }
   };
